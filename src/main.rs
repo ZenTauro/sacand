cf6384391734007915f7e759fe7ea28de97fe3bb
zentauro <pedrogomezmartin1999@gmail.com>
2022-01-23 02:29:53 +0100

    Working implementation

--- src/main.rs
+++ src/main.rs
@@ -6,7 +6,6 @@ use std::fs;
 use alsa::{self, mixer::SelemChannelId};
 use notify_rust::{Notification, NotificationHandle};
 use directories::BaseDirs;
-// use ctrlc;
 
 fn main() {
     let bdir = BaseDirs::new().unwrap();
@@ -60,18 +59,38 @@ fn handle_conn(mut stream: UnixStream, notification: &mut Option<NotificationHan
     let sum: i64 = volumes.iter().copied().sum();
     let avg_vol = sum / (volumes.len() as i64);
 
-    let pct_vol = ((avg_vol as f64) / (vmax as f64)).powf(1.0 / 3.0);
+    let vol = vol_to_pct(avg_vol, vmax);
 
-    // TODO: There is a better way to round up to a decimal place, but
-    // I don't want to add a dependency just for that and I don't feel
-    // like writing the function
-    let vol = format!("{pct_vol:.2}").parse::<f64>().unwrap() * 100.0;
+    let new_vol = match parse_msg(&msg) {
+        Msg::Nop => vol,
+        Msg::Inc(inc) => {
+            let new_vol = vol + (inc as f64);
+            selem.set_playback_volume_all(
+                pct_to_vol(new_vol, vmax)
+            ).expect(&format!("Failed to increment value from {}% to {}%", vol, new_vol));
+            println!("Incrementing value from {}% to {}%", vol, new_vol);
+            println!("Incrementing value from {} to {}", pct_to_vol(vol, vmax), pct_to_vol(new_vol, vmax));
+            new_vol
+        },
+        Msg::Dec(dec) => {
+            let new_vol = vol - (dec as f64);
+            selem.set_playback_volume_all(
+                pct_to_vol(new_vol, vmax)
+            ).expect(&format!("Failed to decrement value from {}% to {}%", vol, new_vol));
+            println!("Decrementing value from {}% to {}%", vol, new_vol);
+            println!("Incrementing value from {} to {}", pct_to_vol(vol, vmax), pct_to_vol(new_vol, vmax));
+            new_vol
+        }
+    };
 
     match notification {
-        Some(handle) => (),
+        Some(notification_handle) => {
+            notification_handle.summary(&format!("{new_vol}%"));
+            notification_handle.update();
+        },
         None => {
             let handle = Notification::new()
-                .summary(&format!("{vol}%"))
+                .summary(&format!("{new_vol}%"))
                 .show()
                 .unwrap();
             *notification = Some(handle);
@@ -79,4 +98,62 @@ fn handle_conn(mut stream: UnixStream, notification: &mut Option<NotificationHan
     }
 
     Ok(())
+}
+
+/// TODO: There is a better way to round up to a decimal place, but
+/// I don't want to add a dependency just for that and I don't feel
+/// like writing the function
+fn pct_to_vol(val: f64, max_val: i64) -> i64 {
+    let frac = val / 100.0;
+    (frac.powi(3) * (max_val as f64)) as i64
+}
+
+/// TODO: There is a better way to round up to a decimal place, but
+/// I don't want to add a dependency just for that and I don't feel
+/// like writing the function
+fn vol_to_pct(val: i64, max_val: i64) -> f64 {
+    let pct_vol = ((val as f64) / (max_val as f64)).powf(1.0 / 3.0);
+    format!("{pct_vol:.2}").parse::<f64>().unwrap() * 100.0
+}
+
+#[derive(Debug, Eq, PartialEq)]
+enum Msg {
+    Inc(u32),
+    Dec(u32),
+    Nop
+}
+
+fn parse_msg(msg: &str) -> Msg {
+    match msg.get(0..1) {
+        Some(op) => match op {
+            "+" => match msg.get(1..) {
+                Some(payload) => {
+                    let num = payload.parse::<u32>()
+                        .unwrap_or(0);
+                    Msg::Inc(num)
+                },
+                None => Msg::Nop
+            },
+            "-" => match msg.get(1..) {
+                Some(payload) => {
+                    let num = payload.parse::<u32>()
+                        .unwrap_or(0);
+                    Msg::Dec(num)
+                },
+                None => Msg::Nop
+            },
+            _   => Msg::Nop,
+        }
+        None => Msg::Nop,
+    }
+}
+
+#[cfg(test)]
+mod test {
+   use super::*;
+
+    #[test]
+    fn parses_ok_plus0() {
+        assert_eq!(parse_msg("+0"), Msg::Inc(0));
+    }
 }
